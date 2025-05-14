fdee452c4a838c7ed6cb236eb51824835002b882
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-05-14 14:07:25 -0600

    Primera carga de datos

---
+++ 20250512-SQLMYSQL2018/all_data.sql
@@ -0,0 +1,4 @@
+/*INGRESAR REGISTRO A LA BASE DE DATOS*/
+INSERT INTO `authors` VALUES(1,'Julio Cortazar','ARG'),
+(2,'Octavio Paz','MEX'),
+(3,'Arthur Conan Doyle', 'GBR');
---
+++ 20250512-SQLMYSQL2018/all_schema.sql
@@ -0,0 +1,8 @@
+/*Eliminar Base de datos*/
+DROP DATABASE IF EXISTS CursoPlatzi;
+
+/*Crear Base de datos*/
+CREATE DATABASE CursoPlatzi;
+
+/*Uilizar base de datos.*/
+USE Cursoplatzi;
