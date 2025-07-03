384245d3c5a54f0f08b097387f3ac36d9987559d
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-07-03 00:06:34 -0600

    Creación de manera correcta tablas con SQL

--- 20250514-FundamentosdBasesdDatos/database/clase2-CreacionTablas.sql
+++ 20250514-FundamentosdBasesdDatos/database/clase2-CreacionTablas.sql
@@ -1,5 +1,8 @@
 CREATE TABLE clients (
+    client_id integer unsigned primary key auto_increment,
     name varchar(100),
-    email varchar(60),
-    phone_number varchar(15)
+    email varchar(60) unique,
+    phone_number varchar(15) not null,
+    created_at timestamp not null default current_timestamp,
+    modified_at timestamp not null default current_timestamp on update current_timestamp
 );
