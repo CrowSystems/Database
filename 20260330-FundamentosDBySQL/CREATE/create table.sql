ff4bb714d9c4fd7fa75ca46f22ea436538dcd1ee
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2026-03-30 22:35:48 -0600

    Carga de tablas de proyecto

--- 20260330-FundamentosDBySQL/CREATE/create table.sql
+++ 20260330-FundamentosDBySQL/CREATE/create table.sql
@@ -1,6 +1,30 @@
 -- CREAR TABLAS.
 -- TABLA SIMPLE SIN LLAVE PRIMARIA SOLO CON TIPOS DE DATOS.
 CREATE TABLE ejemplo_serial(
-    id SERIAL PRIMARY KEY,
-    nombre TEXT NOT NULL
+    id      SERIAL PRIMARY KEY,
+    nombre  TEXT NOT NULL
+);
+
+CREATE TABLE demo_tipos(
+    id      INTEGER,
+    nombre  TEXT
+)
+
+CREATE TABLE Paises(
+    PaisId      SERIAL PRIMARY KEY,
+    CodigoPais VARCHAR(2) NOT NULL,
+    NombrePais TEXT,
+    Continente TEXT
+);
+
+CREATE TABLE Paises(
+    PaisId     INT IDENTITY PRIMARY KEY,
+    CodigoPais VARCHAR(2) NOT NULL,
+    NombrePais TEXT,
+    Continente TEXT
+);
+CREATE TABLE Categorias(
+    CategoriaID     INT IDENTITY(1,1) PRIMARY KEY, 
+    NombreCategoria VARCHAR(150),
+    Descripcion     VARCHAR(500)
 );
